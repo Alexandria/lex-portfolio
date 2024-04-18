8edc6f17f4dc4cdcad4170373287415ab9f15078
Lex <lexluka1031@gmail.com>
2024-04-18 14:10:53 -0500

    3D book prototype

--- src/books.js
+++ src/books.js
@@ -17,16 +17,39 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 /**
- * Create the 3D book
+ * Light
  */
-const geometry = new THREE.BoxGeometry(1.5, 2, 0.25);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const material2 = new THREE.MeshBasicMaterial({ color: "#e042f5" });
+
+const ambientLight = new THREE.AmbientLight("#E5E5E5");
+scene.add(ambientLight);
+
+const light = new THREE.DirectionalLight("#CBC2FF", 0.6);
+scene.add(light);
+
+const loader = new THREE.TextureLoader();
+const urls = [
+  "src/images/edge.jpg",
+  "src/images/bookSpine.webp",
+  "src/images/edge.jpg",
+  "src/images/edge.jpg",
+  "src/images/bookCover.jpg",
+  "src/images/bookCover.jpg",
+];
+
+/**
+ * Create the 3D book materials
+ */
+const geometry = new THREE.BoxGeometry(1.5, 2, 0.2);
+const materials = urls.map((url) => {
+  return new THREE.MeshBasicMaterial({ map: loader.load(url) });
+});
+const material2 = new THREE.MeshToonMaterial({ color: "#e042f5" });
 const material3 = new THREE.MeshBasicMaterial({ color: "#7542f5" });
 const material4 = new THREE.MeshBasicMaterial({ color: "#42c5f5" });
-const cube = new THREE.Mesh(geometry, material);
+
+const cube = new THREE.Mesh(geometry, materials);
 const book2 = new THREE.Mesh(geometry, material2);
-const book3 = new THREE.Mesh(geometry, material3);
+const book3 = new THREE.Mesh(geometry, materials);
 const book4 = new THREE.Mesh(geometry, material4);
 cube.position.x = -6;
 book2.position.x = -3;
@@ -37,6 +60,7 @@ scene.add(cube);
 scene.add(book2);
 scene.add(book3);
 scene.add(book4);
+
 camera.position.z = 5;
 
 const controls = new DragControls(
